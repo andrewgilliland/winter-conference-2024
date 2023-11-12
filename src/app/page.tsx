c3e06477a9559aaf2f61d21a161e98ee7e173543
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-11-12 08:16:35 -0600

    feat: adds winterconference logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
-import DividerHeader from "../components/DividerHeader";
 import SectionHeader from "@/components/SectionHeader";
+import winterConference2024Logo from "../../public/WC24_logo-horiz_RGB 1.svg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <section>
-        <h2 className="font-montserrat text-green-300 text-4xl">
-          Experience the event that will change the way you farm.
-        </h2>
+      <section className="py-16">
+        <div className="flex flex-col items-center gap-6 px-8">
+          <Image
+            src={winterConference2024Logo}
+            alt="Winter Conference 2024 Logo"
+          />
+          <h2 className="font-montserrat text-green-300 text-xl md:text-4xl text-center">
+            Experience the event that will change the way you farm.
+          </h2>
+        </div>
       </section>
 
       <section className="bg-gray-800 py-12">
