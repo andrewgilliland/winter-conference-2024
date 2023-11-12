c9cf27656e65db0fcdf1eaaf9fd54ff3de961308
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-11-12 07:03:35 -0600

    feat: updates typography for tw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="text-green-300 text-4xl ">
-        Experience the event that will change the way you farm.
-      </h2>
+      <section>
+        <h2 className="font-montserrat text-green-300 text-4xl">
+          Experience the event that will change the way you farm.
+        </h2>
+      </section>
+      <section>
+        <h2>The Biggest Event in Precision Ag</h2>
+
+        <p className="font-roboto">
+          Engineers, agronomists, and product experts will share insights
+          focused on improving farm operations with solutions for every season.
+        </p>
+      </section>
+      <section>
+        <h2>21 Locations to Choose From</h2>
+      </section>
+      <section>
+        <h2>Frequently Asked Questions</h2>
+      </section>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["var(--font-montserrat)"],
+        roboto: ["var(--font-roboto)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
