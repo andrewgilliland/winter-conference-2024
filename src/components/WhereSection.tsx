4dea3eb1ec22bf4422e919fb81b7afdc3cb3f2ab
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-11-15 19:21:53 -0600

    refactor: wheresection refactor bg image

--- src/components/WhereSection.tsx
+++ src/components/WhereSection.tsx
@@ -5,36 +5,37 @@ import tremontAudience1 from "../../public/tremont-audience-1.jpeg";
 import map from "../../public/wc24-map-1.jpg";
 import ButtonLink from "./ButtonLink";
 
-const WhereSection = () => {
-  return (
-    <section
-      className="relative bg-center bg-cover z-0"
-      style={{
-        backgroundImage: `url(${tremontAudience1.src})`,
-      }}
-    >
-      <div className="absolute h-full w-full bg-black opacity-80 z-0" />
-      <div className="py-10">
-        <Container>
-          <SectionHeader
-            className="z-10"
-            dividerHeading="Where is Wc24?"
-            mainHeading="21 Locations to Choose From"
-            subHeading="Register today for a location near you!"
-          />
-          <Image
-            className="z-10"
-            src={map}
-            alt="Winter Conference 2024 Locations Map"
-            priority={true}
-          />
-          <div className="hidden md:block">
-            <ButtonLink backgroundColor="transparent" />
-          </div>
-        </Container>
-      </div>
-    </section>
-  );
-};
+const WhereSection = () => (
+  <section className="relative z-0">
+    <Image
+      className="z-[-1] object-cover"
+      src={tremontAudience1}
+      alt="Winter conference audience listening attentively."
+      sizes="(max-width: 768px) 100vw"
+      fill={true}
+      priority={true}
+    />
+    <div className="absolute h-full w-full bg-black opacity-80 z-0" />
+    <div className="py-10">
+      <Container>
+        <SectionHeader
+          className="z-10"
+          dividerHeading="Where is WC24?"
+          mainHeading="21 Locations to Choose From"
+          subHeading="Register today for a location near you!"
+        />
+        <Image
+          className="z-10"
+          src={map}
+          alt="Winter Conference 2024 Locations Map"
+          priority={true}
+        />
+        <div className="hidden md:block">
+          <ButtonLink backgroundColor="transparent" />
+        </div>
+      </Container>
+    </div>
+  </section>
+);
 
 export default WhereSection;
