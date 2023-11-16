99767b6b54c55270ff259d45adfd8d61e54f3a2f
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-11-15 20:11:20 -0600

    feat: updates responsive headings

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -43,12 +43,12 @@ const MediaCard = ({
       <div
         className={`flex flex-col justify-center lg:w-[55%] bg-gray-700 border-b-8 lg:border-b-0 ${
           isEven ? "lg:border-r-8 lg:-ml-9" : "lg:border-l-8 lg:-mr-9 z-10"
-        } border-green-300 mx-4 md:mx-8 lg:mx-0 -mt-9 lg:mt-0 pt-4 lg:pt-0 px-6 pb-9 lg:pb-0 gap-2 max-w-[280px] md:max-w-none lg:h-[195px] xl:h-[230px] shadow`}
+        } border-green-300 mx-4 md:mx-8 lg:mx-0 -mt-9 lg:mt-0 pt-4 lg:pt-0 px-6 xl:px-16 pb-9 lg:pb-0 gap-2 xl:gap-5 max-w-[280px] md:max-w-none lg:h-[200px] xl:h-[230px] shadow`}
       >
-        <h4 className="font-montserrat font-bold text-xl text-white">
+        <h4 className="font-montserrat font-bold text-xl xl:text-2xl text-white">
           {heading}
         </h4>
-        <p className="font-roboto text-lg">{copy}</p>
+        <p className="font-roboto text-lg xl:text-xl">{copy}</p>
       </div>
     </div>
   );
